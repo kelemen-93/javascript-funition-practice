b6789a75c8d4279e81bd5602cc8ac8b0995f7021
Kelemen Gábor <kgabesz18@gmail.com>
2020-07-12 17:17:27 +0200

    finished the first assignment

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+function triangle(positionX, positionY, size) {
+    
+  context.beginPath();
+  context.moveTo(positionX, positionY);
+  context.lineTo(positionX - size / 2, positionY + size);
+  context.lineTo(positionX + size / 2, positionY + size);
+  context.closePath();
+  
+  context.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+  context.stroke();
+  
+  context.fillStyle = 'rgba(255, 165, 0, 0.5)';
+  context.fill();
+  
+}
+
+triangle(230, 160, 50);
+triangle(270, 100, 50);
+triangle(200, 50, 150);
